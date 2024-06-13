de0f40a7e842eba2f0448632574fb18b3d067b34
Ranjithjo19 <ranjithjo19@gmail.com>
2024-06-13 06:16:54 +0000

    timeline update

---
+++ src/components/ProjectTimelineCard/index.js
@@ -0,0 +1,46 @@
+import {AiFillCalendar} from 'react-icons/ai'
+
+import './index.css'
+/*
+import {
+  ProjectCardContainer,
+  ProjectImage,
+  ProjectTitleAndDurationContainer,
+  ProjectTitle,
+  DurationContainer,
+  Duration,
+  ProjectDescription,
+  VisitLink,
+} from './styledComponents'
+
+*/
+
+const ProjectTimelineCard = props => {
+  const {projectDetails} = props
+  const {
+    imageUrl,
+    projectTitle,
+    description,
+    projectUrl,
+    duration,
+  } = projectDetails
+
+  return (
+    <div className="ProjectCardContainer">
+      <img className="ProjectImage" src={imageUrl} alt="project" />
+      <div className="ProjectTitleAndDurationContainer">
+        <h1 className="ProjectTitle">{projectTitle}</h1>
+        <div className="DurationContainer">
+          <AiFillCalendar color="#171f46" />
+          <p className="Duration">{duration}</p>
+        </div>
+      </div>
+      <p className="ProjectDescription">{description}</p>
+      <a className="VisitLink" href={projectUrl}>
+        Visit
+      </a>
+    </div>
+  )
+}
+
+export default ProjectTimelineCard
